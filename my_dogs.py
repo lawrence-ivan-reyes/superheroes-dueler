fc792257ed960d245c4439794ce68d6fc2173a86
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-11-11 19:27:59 -0500

    removed comments in my_dogs.py

--- my_dogs.py
+++ my_dogs.py
@@ -6,13 +6,6 @@ my_dog.bark()
 my_other_dog = Dog("Annie", "SuperDog")
 print(my_other_dog.name)
 
-# Make three dogs with fun names and breeds
-# Write two new methods to have the dogs sit and roll over (just print "<<DOG'S NAME>> sits", "<<DOG'S NAME>> rolls over").
-# Have one dog bark, one sit, and one roll over.
-# Notice how each dog displays its unique name when you print its name property. Do you remember why this is?
-
-# Notice how each dog makes the same sound "Woof!" when it barks. Why is this?
-
 harry = Dog("Harry", "Gryffindog")
 harry.bark()
 
