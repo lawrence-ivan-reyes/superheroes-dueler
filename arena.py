d959672b31785dbc17bb327acaf0529b9831a434
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-11-17 20:54:38 -0500

    added game loop in Arena class

--- arena.py
+++ arena.py
@@ -84,8 +84,19 @@ class Arena:
                 print(f"Survived from {self.team_two.name}: {hero.name}")
 
 if __name__ == "__main__":
+    game_is_running = True
     arena = Arena()
+
     arena.build_team_one()
     arena.build_team_two()
-    arena.team_battle()
-    arena.show_stats()
+
+    while game_is_running:
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
+
+        if play_again.lower() == "n":
+            game_is_running = False
+        else:
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
